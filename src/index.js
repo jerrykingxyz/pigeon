10e2ffbd95abb70b49ac3a49d3f25c1fb173983d
jerrykingxyz <jerrykingxyz@gmail.com>
2019-03-10 22:48:48 +0800

    feat : pigeon add events

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Channel = require('./channel');
 class Pigeon {
   constructor() {
     this.channels = {};
+    this.events = {};
   }
 
   addChannel(instance) {
@@ -22,11 +23,61 @@ class Pigeon {
   }
 
   async send(letter) {
+    if (typeof letter !== 'object') {
+      throw new Error('letter must be a object');
+    }
+
     const channel = this.channels[letter.channel];
     if (!channel) {
       throw new Error('channel not exist');
     }
+
     return channel.sendMessage(letter.msg);
+  }
+
+  on(eventName, callback) {
+    if (typeof eventName !== 'string') {
+      throw new Error('event name must be string');
+    }
+    if (typeof callback !== 'function') {
+      throw new Error('event callback must be a function');
+    }
+
+    if (!Array.isArray(this.events[eventName])) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+  }
+
+  off(eventName, callback) {
+    if (typeof eventName !== 'string') {
+      throw new Error('event name must be string');
+    }
+
+    if (typeof callback === 'undefined') {
+      delete this.events[eventName];
+      return;
+    }
+    const events = this.events[eventName];
+    if (!Array.isArray(events)) {
+      return;
+    }
+    const index = events.indexOf(callback);
+    if (index !== -1) {
+      events.splice(index, 1);
+    }
+  }
+
+  emit(eventName) {
+    if (typeof eventName !== 'string') {
+      throw new Error('event name must be string');
+    }
+
+    const events = this.events[eventName];
+    if (!Array.isArray(events)) {
+      return;
+    }
+    events.forEach(e => e());
   }
 }
 
